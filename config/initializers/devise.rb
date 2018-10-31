a4a2f76b1394f975ce3edaa5bf8c65618e2b6706
Fish-bowl <symbolt.ap@gmail.com>
2018-10-31 13:07:43 -0600

    added super secret key

---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+Devise.setup do |config|
+  config.secret_key = 'cc67ce05316c5513a20fca6b3c70cdc00ea3b182b697aabca67a90036ade64fe9eb857002b15cab136bcbe30b900063e08a9d049c02cb82179f1d76a92e35ad6'
+end
--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 DeviseTokenAuth.setup do |config|
-  config.secret_key = 'cc67ce05316c5513a20fca6b3c70cdc00ea3b182b697aabca67a90036ade64fe9eb857002b15cab136bcbe30b900063e08a9d049c02cb82179f1d76a92e35ad6'
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
