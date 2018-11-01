b95b7aefb216e4c83963779e4f0f58a1e2b1f943
Fish-bowl <symbolt.ap@gmail.com>
2018-10-31 18:20:13 -0600

    made icons mobile

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,56 +11,6 @@ import {
 import FilesSVG from '../images/sql.svg'
 import RailsPNG from '../images/rails.png'
 
-const styles = {
-  container: {
-    minHeight: '100vh',
-  },
-
-  grid: {
-    position: 'inherit',
-  },
-
-  segments: {
-    position: 'inherit',
-  },
-
-  header: {
-    color: 'white',
-    textAlign: 'center',
-    posistion: 'absolute',
-  },
-
-  paragraphs: {
-    color: 'white',
-    fontSize: '1.5em',
-  },
-
-  iconRow: {
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-
-  iconFakes: {
-    height: '56px',
-    width: '66px',
-    paddingLeft: '10px',
-    paddingRight: '10px',
-  }, 
-
-  titleImage: {
-    position: 'inherit',
-    height: '300px',
-    width: '100%',
-    marginTop: '45px',
-  },
-
-  test: {
-    border: 'solid 3px red',
-    color: 'white',
-    position: 'absolute'
-  }
-}
-
 class Home extends React.Component {
   render() {
     return(
@@ -74,7 +24,7 @@ class Home extends React.Component {
           </Header>
         </Segment>
         <Container>
-          <Grid divided centered style={styles.grid} >
+          <Grid verticalAlign="middle" stackable divided centered style={styles.grid} >
             <Grid.Row>
               <Grid.Column width={8} >
                 <Segment basic style={styles.segments} >
@@ -150,67 +100,182 @@ class Home extends React.Component {
               </Grid.Column>
             </Grid.Row>
             <Divider />
-            <Grid.Row 
-              divided 
-              style={styles.iconRow} 
-            >
-              <Icon 
-                color='grey' 
-                size='huge' 
-                name='github' 
-                inverted 
-              />
-              <Icon 
-                color='orange' 
-                size='huge'  
-                name='html5' 
-                inverted 
-              />
-              <Icon 
-                color='blue' 
-                size='huge' 
-                name='css3' 
-                inverted 
-              />
-              <Icon color='yellow' size='huge' name='js square' inverted />
-              <Icon color='red' size='huge' name='gem outline' inverted />
-              <Image
-                alt='ruby on rails'
-                src={RailsPNG}
-                style={styles.iconFakes}
-              />
-              <Image
-                alt='sql stack'
-                src={FilesSVG}
-                style={styles.iconFakes}
-              />
+            <Grid.Row divided centered columns={7} style={styles.iconRow}  >
+              <Grid.Column style={styles.icons} >
+                <Icon
+                  color='orange'
+                  size='huge'
+                  name='html5'
+                  inverted
+                />
+              </Grid.Column>
+              <Grid.Column style={styles.icons} >
+                <Icon
+                  color='blue'
+                  size='huge'
+                  name='css3'
+                  inverted
+                />
+              </Grid.Column>
+              <Grid.Column style={styles.icons} >
+                <Icon color='yellow' size='huge' name='js square' inverted />
+              </Grid.Column>
+              <Grid.Column style={styles.icons} >
+                <Icon color='red' size='huge' name='gem outline' inverted />
+              </Grid.Column>
+              <Grid.Column style={styles.icons} >
+                <Image
+                  alt='ruby on rails'
+                  src={RailsPNG}
+                  style={styles.iconFakes}
+                />
+              </Grid.Column>
+              <Grid.Column style={styles.icons} >
+                <Icon
+                  color='grey'
+                  size='huge'
+                  name='github'
+                  inverted
+                />
+              </Grid.Column>
+              <Grid.Column style={styles.icons} >
+                <Image
+                  alt='sql stack'
+                  src={FilesSVG}
+                  style={styles.iconFakes}
+                />
+              </Grid.Column>
             </Grid.Row>
             <Divider />
-            <Grid.Row columns={3} >
-              <Grid.Column width={4} >
-                <Segment basic  >
-                  <Header style={styles.header} as='h1'>this will be one 'skill card'</Header>
-                </Segment>
-              </Grid.Column>
-              <Grid.Column width={4} >
-                <Segment basic  >
-                  <Header style={styles.header} as='h1'>this will be one 'skill card'</Header>
-                </Segment>
-              </Grid.Column>
-              <Grid.Column width={4} >
-                <Segment basic  >
-                  <Header style={styles.header} as='h1'>this will be one 'skill card'</Header>
-                </Segment>
-              </Grid.Column>
+            <Grid.Row style={styles.bottomRow} >
+              <Segment basic >
+              </Segment>
             </Grid.Row>
           </Grid>
-          <Divider />
-          
-            
-        
         </Container>
       </div>
     )
+  }
+}
+
+{/* <Grid.Row
+  divided
+  style={styles.iconRow}
+  columns={2}
+>
+  <Grid.Column style={styles.iconRow} verticalAlign='center' largeScreen={8} mobile={16} >
+    <Icon
+      color='orange'
+      size='huge'
+      name='html5'
+      inverted
+    />
+    <Icon
+      color='blue'
+      size='huge'
+      name='css3'
+      inverted
+    />
+    <Icon color='yellow' size='huge' name='js square' inverted />
+  </Grid.Column>
+  <Grid.Column style={styles.iconRow} verticalAlign='center' largeScreen={8} mobile={16} >
+    <Icon color='red' size='huge' name='gem outline' inverted />
+    <Image
+      alt='ruby on rails'
+      src={RailsPNG}
+      style={styles.iconFakes}
+    />
+    <Icon
+      color='grey'
+      size='huge'
+      name='github'
+      inverted
+    />
+    <Image
+      alt='sql stack'
+      src={FilesSVG}
+      style={styles.iconFakes}
+    />
+  </Grid.Column>
+</Grid.Row>
+  <Divider />
+  <Grid.Row columns={3} >
+    <Grid.Column width={4} >
+      <Segment basic  >
+        <Header style={styles.header} as='h1'>this will be one 'skill card'</Header>
+      </Segment>
+    </Grid.Column>
+    <Grid.Column width={4} >
+      <Segment basic  >
+        <Header style={styles.header} as='h1'>this will be one 'skill card'</Header>
+      </Segment>
+    </Grid.Column>
+    <Grid.Column width={4} >
+      <Segment basic  >
+        <Header style={styles.header} as='h1'>this will be one 'skill card'</Header>
+      </Segment>
+    </Grid.Column>
+  </Grid.Row> */}
+
+const styles = {
+  container: {
+    minHeight: '100vh',
+  },
+
+  grid: {
+    position: 'inherit',
+  },
+  
+  segments: {
+    position: 'inherit',
+  },
+  
+  header: {
+    color: 'white',
+    textAlign: 'center',
+    posistion: 'absolute',
+  },
+  
+  titleImage: {
+    position: 'inherit',
+    height: '300px',
+    width: '100%',
+    marginTop: '45px',
+  },
+
+  paragraphs: {
+    color: 'white',
+    fontSize: '1.5em',
+  },
+
+  iconRow: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+
+  icons: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+
+  iconFakes: {
+    height: '56px',
+    width: '66px',
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+
+  bottomRow: {
+    marginBottom: '5%',
+
+  },
+
+  test: {
+    border: 'solid 3px red',
+    color: 'white',
+    position: 'absolute'
   }
 }
 
